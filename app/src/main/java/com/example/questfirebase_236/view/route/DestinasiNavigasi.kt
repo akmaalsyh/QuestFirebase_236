cc382d19e3a1c3819f7c8fd108927d1e5ac4171f
akmaalsyh <taufansyah50@gmail.com>
2026-01-02 23:13:52 +0700

    membuat interface untuk destinasi navigasi

---
+++ app/src/main/java/com/example/questfirebase_236/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,9 @@
+package com.example.questfirebase_236.view.route
+
+/**
+ * Interface untuk mendefinisikan rute navigasi dalam aplikasi.
+ */
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: Int
+}
