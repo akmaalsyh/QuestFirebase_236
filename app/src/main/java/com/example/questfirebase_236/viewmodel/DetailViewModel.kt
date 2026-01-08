cfa9f0a24be5dbc21ed29ebec371e77f0534f50c
akmaalsyh <taufansyah50@gmail.com>
2026-01-08 18:30:41 +0700

    membuat status UI untuk detail siswa

--- app/src/main/java/com/example/questfirebase_236/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_236/viewmodel/DetailViewModel.kt
@@ -1,3 +1,5 @@
+@file:OptIn(InternalSerializationApi::class)
+
 package com.example.questfirebase_236.viewmodel
 
 import androidx.compose.runtime.getValue
@@ -7,38 +9,54 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.questfirebase_236.modeldata.Siswa
-import com.example.questfirebase_236.repositori.RepositorySiswa
-import com.example.questfirebase_236.view.route.DestinasiDetail
+import com.example.questfirebase_236.repository.RepositorySiswa
 import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satuSiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
 
 class DetailViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositorySiswa: RepositorySiswa
 ) : ViewModel() {
-    private val itemId: String = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
 
-    var detailUiState by mutableStateOf(DetailUiState())
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
 
     init {
-        getSiswaById()
+        getSatuSiswa()
     }
 
-    private fun getSiswaById() {
+    fun getSatuSiswa() {
         viewModelScope.launch {
-            detailUiState = DetailUiState(isLoading = true)
-            try {
-                val result = repositorySiswa.getSiswaById(itemId)
-                detailUiState = DetailUiState(detailSiswa = result)
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(
+                    satuSiswa = repositorySiswa.getSatuSiswa(idSiswa)
+                )
+            } catch (e: IOException) {
+                StatusUIDetail.Error
             } catch (e: Exception) {
-                detailUiState = DetailUiState(isError = true)
+                StatusUIDetail.Error
             }
         }
     }
 
-    data class DetailUiState(
-        val detailSiswa: Siswa = Siswa(),
-        val isLoading: Boolean = false,
-        val isError: Boolean = false
-    )
+    suspend fun hapusSatuSiswa() {
+        try {
+            repositorySiswa.hapusSatuSiswa(idSiswa)
+            println("Sukses Hapus Data: $idSiswa")
+        } catch (e: Exception) {
+            println("Gagal Hapus Data: ${e.message}")
+        }
+    }
 }
